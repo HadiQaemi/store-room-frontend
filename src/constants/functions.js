6b8161d213c25c25d47dc0136121eeac88a49c84
hadi <hadi.qaemi@gmail.com>
2022-04-01 19:28:25 +0430

    add toast function

--- src/constants/functions.js
+++ src/constants/functions.js
@@ -1,7 +1,33 @@
+import Swal from 'sweetalert2'
+
 export const toastAnyWhere = {
   show: function (message = '', variant = 'success', autoHideDuration = 5000) {
     if (this.display) {
       this.display.show(message, variant, autoHideDuration)
     }
+  },
+}
+export const Toast = {
+  show: function (
+    message = '',
+    variant = 'success',
+    autoHideDuration = 5000,
+    position = 'top-end',
+  ) {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: position,
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      },
+    })
+    Toast.fire({
+      icon: variant,
+      title: message,
+    })
   },
 }
